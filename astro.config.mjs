760046007adc2d6cdeae0fd43039ab9d46dadc09
Prince Singh <marvelousprince012233@gmail.com>
2024-02-09 03:14:01 +0530

    added vercel adaptor

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,25 +7,21 @@ import { sanityIntegration } from "@sanity/astro";
 import sanity from "@sanity/astro";
 import react from "@astrojs/react";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astroship-pro.web3templates.com",
+  site: "https://favdevs.vercel.app/",
   image: {
-    domains: ["source.unsplash.com", "images.unsplash.com"],
+    domains: ["source.unsplash.com", "images.unsplash.com"]
   },
   output: "hybrid",
-  integrations: [
-    tailwind(),
-    mdx(),
-    icon(),
-    sitemap(),
-    sanityIntegration({
-      projectId: "btb9magv",
-      dataset: "production",
-      // Set useCdn to false if you're building statically.
-      useCdn: false,
-      studioBasePath: "/admin",
-    }),
-    react(),
-  ],
+  adapter: vercel(),
+  integrations: [tailwind(), mdx(), icon(), sitemap(), sanityIntegration({
+    projectId: "btb9magv",
+    dataset: "production",
+    // Set useCdn to false if you're building statically.
+    useCdn: false,
+    studioBasePath: "/admin"
+  }), react()]
 });
